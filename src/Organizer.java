bd4683be3940709b7acaecc79489b4bf0023d525
J-Champagne <slapjack.dev@gmail.com>
2016-08-31 11:27:41 -0400

    Initial commit

---
+++ src/Organizer.java
@@ -0,0 +1,17 @@
+/**
+ * Created by jackchampagne on 8/31/16.
+ * V Alpha 0.0.1
+ *
+ * Application will
+ * + organize projects and daily homework
+ * + show school day
+ * + show classes for the current day
+ *
+ * STRETCH GOALS:
+ * + Local and CLoud saving
+ * + Server to hold cloud saves.
+ */
+
+public class Organizer {
+
+}
